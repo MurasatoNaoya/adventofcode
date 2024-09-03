67837486c76009180ccfbf8db0a16785305619aa
MurasatoNaoya <sakuralondon101z@gmail.com>
2024-09-03 18:36:10 +0100

    Completed basic problem functionality.

--- 2023/cube_conundrum.py
+++ 2023/cube_conundrum.py
@@ -37,24 +37,50 @@ def parse_game(rounds:str) -> list[list[tuple[int, str]]]:
 
 
     
-def check_valid_round(turn:list[tuple[int, str]], actual: list[tuple[int, str]]) -> bool:
+def check_valid_round(turn:list[tuple[int, str]], actual: dict) -> bool:
     '''Returns bool for whether a given round is valid, given starting list of cubes.'''
 
-    ... 
+    for draw in turn:
+        print(draw)
+        count, colour = draw
+        # Count must be converted to an int, 
+        # as it if from a .txt file, which only
+        # contains strings.
+        if actual[colour] < int(count):
+            return False
+    return True
 
 
-def check_valid_game(game:list[list[tuple[int, str]]], actual: list[tuple[int, str]]) -> bool:
+
+def check_valid_game(game:list[list[tuple[int, str]]], actual: dict) -> bool:
     '''Returns bool for whether a given game is valid, given starting list of cubes.'''
 
-    ... 
+    for round in game:
+        if not check_valid_round(round, actual):
+            return False
+    return True  
 
 
-def count_valid_games(games:list[list[list[tuple[int, str]]]]) -> int:
-    '''Returns number of valid games, for a given games array.'''
+# def count_valid_games(games:list[list[list[tuple[int, str]]]]) -> int:
+#     '''Returns number of valid games, for a given games array.'''
 
-    ... 
+
+
+def count_id_sum():
+    ...
+
 
 
 if __name__ == '__main__':
+
+    actual_draw = {
+        'red' : 12,
+        'green' : 13,
+        'blue' : 14
+    
+    }
+
+
     id, rounds = extract_id_game("Game 1: 7 blue, 6 green, 3 red; 3 red, 5 green, 1 blue; 1 red, 5 green, 8 blue; 3 red, 1 green, 5 blue")
-    print(parse_game(rounds))
+    game = parse_game(rounds)
+    print(check_valid_game(game, actual=actual_draw))
